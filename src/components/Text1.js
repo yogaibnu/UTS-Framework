aff15304ffbcaa8e2a760cb1c59c2d63d36589bd
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 09:24:24 +0700

    change Text1

--- src/components/Text1.js
+++ src/components/Text1.js
@@ -1,9 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Text1 extends Component {
-    render() {
-        return <p>Hello from Text1</p>
-    }
-}
+const Text1 = () => {
+    return <p>Hello from Text1 as function component</p>
+};
 
 export default Text1;
