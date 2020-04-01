2235f947d08780abb0b078e28de74dd9a459d899
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 15:26:05 +0700

    create MoviesContainer

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
 
+import {connect} from 'react-redux';
+
 import SearchForm from './SearchForm';
+import MoviesContainer from './MoviesContainer';
+
 
 export class Landing extends Component {
     render() {
+        const {loading} = this.props;
         return (
             <div className="container">
                 <SearchForm />
+                {loading ? <Spinner /> : <MoviesContainer />}
             </div>
-        )
+        );
     }
 }
 
-export default Landing
+const mapStateToProps = state => ({
+    loading: state.movies.loading
+})
+
+export default connect(mapStateToProps)(Landing);
---
+++ src/components/home/MovieCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class MovieCard extends Component {
+    render() {
+        const movie = this.props;
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default MovieCard
