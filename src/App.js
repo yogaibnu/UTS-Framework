a2882b567e1dcc9da856aacce7a87e0f43a7b129
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 16:47:27 +0700

    create componentDidMount in movie.js + using route in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/home/Landing';
+import Movie from './components/home/Movie';
 import store from './store';
 
 class App extends Component {
@@ -16,7 +17,8 @@ class App extends Component {
         <Router>
           <div>
             <Navbar />
-            <Landing />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/movie/:id" component={Movie} />
             <Footer />
           </div>
         </Router>
--- src/components/home/Movie.js
+++ src/components/home/Movie.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
+
+import {fetchMovie} from '../../actions/searchActions';
 
 export class Movie extends Component {
+    componentDidMount() {
+        this.props.fetchMovie(this.props.match.params.id)
+    }
+    
     render() {
         return (
             <React.Fragment>
@@ -58,4 +65,9 @@ export class Movie extends Component {
     }
 }
 
-export default Movie
+const mapStateToProps = state => ({
+    loading: state.movies.loading,
+    movie: state.movies.movie
+})
+
+export default connect(mapStateToProps, {fetchMovie})(Movie);
