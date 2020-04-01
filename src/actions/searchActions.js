eb8ca2374bd6b5123e91ae75cc81a195ad3f44f8
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 14:29:03 +0700

    add function fetchMovies

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,8 +1,21 @@
-import {SEARCH_MOVIE} from './types';
+import { SEARCH_MOVIE, FETCH_MOVIES } from './types';
+import axios from 'axios';
+
+import { APIKey } from '../APIKey';
 
 export const searchMovie = text => dispatch => {
     dispatch({
         type: SEARCH_MOVIE,
         payload: text
-    })
-}
+    });
+};
+
+export const fetchMoves = text => dispatch => {
+    axios
+        .get(`http://www.omdbapi.com/?apikey=${APIKey}&s=${text}`)
+        .then(response => dispatch({
+            type: FETCH_MOVIES,
+            payload: response.data
+        }))
+        .catch(err => console.log(err));
+};
