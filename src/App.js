1ae342008135f6bbf6e37e6ad2df0cc9b196b097
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 09:20:25 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <Text1 />;
+  }
 }
 
 export default App;
