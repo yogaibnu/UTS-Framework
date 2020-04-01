4a18d28e1115865e78ff9c966699a0bf626ffed2
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 13:24:24 +0700

    create Landing.js + import in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Landing from './components/home/Landing';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Landing />
         <Footer />
       </div>
     );
---
+++ src/components/home/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import SearchForm from './SearchForm';
+
+export class Landing extends Component {
+    render() {
+        return (
+            <div className="container">
+                <SearchForm />
+            </div>
+        )
+    }
+}
+
+export default Landing
