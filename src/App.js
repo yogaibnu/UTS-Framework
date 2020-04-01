6319080577c48bd9b6d56e3bfbdfc96bf5435bd9
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 10:57:15 +0700

    import footer in app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
-    return <Navbar />;
-  }
+    return
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+  };
 }
 
 export default App;
