de5435d4bd117865c5bb7c01d1ce934c2f6894e5
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 15:07:05 +0700

    create Spinner.js

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,7 +4,11 @@ import spinner from './spinner.gif';
 function Spinner() {
     return (
         <div>
-            
+            <img
+                src={spinner}
+                style={{ width: '200px', margin: 'auto', display: 'block' }}
+                alt="Loading...">
+            </img>
         </div>
     )
 }
