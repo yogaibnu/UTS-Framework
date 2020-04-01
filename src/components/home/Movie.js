07641668c6f9ecc11d8d9d6806aa889864113dcb
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 18:00:34 +0700

    add container in Movie.js

--- src/components/home/Movie.js
+++ src/components/home/Movie.js
@@ -15,59 +15,61 @@ export class Movie extends Component {
     render() {
         const { loading, movie } = this.props;
 
-        let movieInfo = (<React.Fragment>
-            <div className="row">
-                <div className="col-md-4 card card-body">
-                    <img src={movie.Poster} className="thumbnail" alt="Poster"></img>
+        let movieInfo = (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 card card-body">
+                        <img src={movie.Poster} className="thumbnail" alt="Poster"></img>
+                    </div>
+                    <div className="col-md-8">
+                        <h2 className="mb-4">{movie.Title}</h2>
+                        <ul className="list-group">
+                            <li className="list-group-item">
+                                <strong>Genre:</strong> {movie.Genre}
+                            </li>
+                            <li className="list-group-item">
+                                <strong>Released:</strong> {movie.Released}
+                            </li>
+                            <li className="list-group-item">
+                                <strong>Rated:</strong> {movie.Rated}
+                            </li>
+                            <li className="list-group-item">
+                                <strong>IMDB Rating:</strong> {movie.imdbRating}
+                            </li>
+                            <li className="list-group-item">
+                                <strong>Director:</strong> {movie.Director}
+                            </li>
+                            <li className="list-group-item">
+                                <strong>Writer:</strong> {movie.Writer}
+                            </li>
+                            <li className="list-group-item">
+                                <strong>Actors:</strong> {movie.Actors}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="col-md-8">
-                    <h2 className="mb-4">{movie.Title}</h2>
-                    <ul className="list-group">
-                        <li className="list-group-item">
-                            <strong>Genre:</strong> {movie.Genre}
-                        </li>
-                        <li className="list-group-item">
-                            <strong>Released:</strong> {movie.Released}
-                        </li>
-                        <li className="list-group-item">
-                            <strong>Rated:</strong> {movie.Rated}
-                        </li>
-                        <li className="list-group-item">
-                            <strong>IMDB Rating:</strong> {movie.imdbRating}
-                        </li>
-                        <li className="list-group-item">
-                            <strong>Director:</strong> {movie.Director}
-                        </li>
-                        <li className="list-group-item">
-                            <strong>Writer:</strong> {movie.Writer}
-                        </li>
-                        <li className="list-group-item">
-                            <strong>Actors:</strong> {movie.Actors}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className="row">
-                <div className="card card-body bg-dark my-5 text-light">
-                    <div className="cold-md-12">
-                        <h3>About</h3>
-                        {movie.Plot}
-                        <br></br>
-                        <br></br>
-                        <a
-                            href={'https://www.imdb.com/title' + movie.imdID}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="btn btn-primary">
-                            View on IMDB
+                <div className="row">
+                    <div className="card card-body bg-dark my-5 text-light">
+                        <div className="cold-md-12">
+                            <h3>About</h3>
+                            {movie.Plot}
+                            <br></br>
+                            <br></br>
+                            <a
+                                href={'https://www.imdb.com/title/' + movie.imdbID}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="btn btn-primary">
+                                View on IMDB
                     </a>
-                        <Link to="/" className="btn btn-default text-light">
-                            Go Back To Search
+                            <Link to="/" className="btn btn-default text-light">
+                                Go Back To Search
                     </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </React.Fragment>)
+        )
 
         let content = loading ? <Spinner /> : movieInfo;
         return (
