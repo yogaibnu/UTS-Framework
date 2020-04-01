a32490c6db9af6a1ca867020d61cdeb8451ffb88
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 09:21:34 +0700

    add import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+import Text1 from './components/Text1';
+
 class App extends Component {
   render() {
     return <Text1 />;
