718a0a6e2f8e8e9e705da0716cec1a7f056265d3
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-01 13:12:27 +0700

    change loc

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 class App extends Component {
   render() {
